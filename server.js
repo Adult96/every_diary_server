2ab393e4ad301021238bc753db52f90cdf74c9d6
Adult96 <djxor007@gmail.com>
2023-02-21 17:27:18 +0900

    feat: jsonserver 추가

---
+++ server.js
@@ -0,0 +1,27 @@
+const jsonServer = require('json-server');
+const path = require('path');
+
+const fs = require('fs');
+const os = require('os');
+
+fs.copyFile('db.json', os.tmpdir() + '/db.json', function (err) {
+  if (err) console.log(err);
+  else console.log('copy file succeed to' + os.tmpdir());
+});
+
+const server = jsonServer.create();
+const router = jsonServer.router(path.resolve(os.tmpdir() + '/db.json'));
+const middlewares = jsonServer.defaults({
+  static: path.resolve(__dirname + '/../build/'),
+});
+
+const port = process.env.PORT || 3001;
+
+server.use(middlewares);
+
+server.use(jsonServer.bodyParser);
+
+server.use(router);
+server.listen(port, () => {
+  console.log('JSON Server is running');
+});
